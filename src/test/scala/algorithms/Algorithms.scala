603fe07dae46a03b0674e153144c711561d76140
aburns <anthony@clearme.com>
2016-02-28 09:26:28 -0500

    simplified test created Trait

--- src/test/scala/algorithms/Algorithms.scala
+++ src/test/scala/algorithms/Algorithms.scala
@@ -7,7 +7,9 @@ import org.scalatest.FreeSpec
 class Algorithms extends FreeSpec with HRTest {
   "Algorithms https://www.hackerrank.com/domains/algorithms" - {
     "Solve Me First https://www.hackerrank.com/challenges/solve-me-first/submissions/code/17889169" in {
-      testSolution("3\n2\n", "5\n", SolveMeFirst.Solution.main)
+      testSolution("3\n2\n",
+        "5\n",
+        SolveMeFirst.Solution.main)
     }
   }
 }
